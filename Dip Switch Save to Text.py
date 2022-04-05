455769307c084dc90a7c728293098decfa0482ef
VincentLiem <96896686+VincentLiem@users.noreply.github.com>
2022-04-05 11:34:25 -0700

    Update Dip Switch Save to Text.py

--- Dip Switch Save to Text.py
+++ Dip Switch Save to Text.py
@@ -1,7 +1,8 @@
-# save dip switch setup to text file
+import csv
+
 device = input("Enter device's name >> ")
-print("This program only works on devices with up to 10 switches.")
-dip_on = input("Enter dip switches that are on >> ")
+print('This program only works on devices with up to 10 switches.')
+dip_on = input('Enter dip switches that are on >> ')
 dip1 = False
 dip2 = False
 dip3 = False
@@ -13,38 +14,60 @@ dip8 = False
 dip9 = False
 dip10 = False
 dip_on = str(dip_on)
-if "10" in dip_on:
+if '10' in dip_on:
     dip10 = True
     dip_on = dip_on.replace('10', '')
-if "1" in dip_on:
+if '1' in dip_on:
     dip1 = True
-if "2" in dip_on:
+if '2' in dip_on:
     dip2 = True
-if "3" in dip_on:
+if '3' in dip_on:
     dip3 = True
-if "4" in dip_on:
+if '4' in dip_on:
     dip4 = True
-if "5" in dip_on:
+if '5' in dip_on:
     dip5 = True
-if "6" in dip_on:
+if '6' in dip_on:
     dip6 = True
-if "7" in dip_on:
+if '7' in dip_on:
     dip7 = True
-if "8" in dip_on:
+if '8' in dip_on:
     dip8 = True
-if "9" in dip_on:
+if '9' in dip_on:
     dip9 = True
 
+def SaveText():
+        with open(device + ' Dip.txt', 'w') as save:
+            save.write('Dip 1: ' + str(dip1) + '\n')
+            save.write('Dip 2: ' + str(dip2) + '\n')
+            save.write('Dip 3: ' + str(dip3) + '\n')
+            save.write('Dip 4: ' + str(dip4) + '\n')
+            save.write('Dip 5: ' + str(dip5) + '\n')
+            save.write('Dip 6: ' + str(dip6) + '\n')
+            save.write('Dip 7: ' + str(dip7) + '\n')
+            save.write('Dip 8: ' + str(dip8) + '\n')
+            save.write('Dip 9: ' + str(dip9) + '\n')
+            save.write('Dip 10: ' + str(dip10) + '\n')
+        
+def SaveCSV():
+        with open(device + ' Dip.csv', 'w') as save:
+            writer = csv.writer(save)
+            writer.writerow(['Dip1', dip1])
+            writer.writerow(['Dip2', dip2])
+            writer.writerow(['Dip3', dip3])
+            writer.writerow(['Dip4', dip4])
+            writer.writerow(['Dip5', dip5])
+            writer.writerow(['Dip6', dip6])
+            writer.writerow(['Dip7', dip7])
+            writer.writerow(['Dip8', dip8])
+            writer.writerow(['Dip9', dip9])
+            writer.writerow(['Dip10', dip10])
 
-with open(device + " Dip.txt", "w") as save:
-    save.write("Dip 1: " + str(dip1) + "\n")
-    save.write("Dip 2: " + str(dip2) + "\n")
-    save.write("Dip 3: " + str(dip3) + "\n")
-    save.write("Dip 4: " + str(dip4) + "\n")
-    save.write("Dip 5: " + str(dip5) + "\n")
-    save.write("Dip 6: " + str(dip6) + "\n")
-    save.write("Dip 7: " + str(dip7) + "\n")
-    save.write("Dip 8: " + str(dip8) + "\n")
-    save.write("Dip 9: " + str(dip9) + "\n")
-    save.write("Dip 10: " + str(dip10) + "\n")
-save.close()
+save_format = input('Save as text, csv, or both? >> ')
+if save_format.lower() == 'text':
+    SaveText()
+elif save_format.lower() == 'csv':
+    SaveCSV()
+else:
+    SaveText()
+    SaveCSV()
